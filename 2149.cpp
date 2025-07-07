599a13acacfa5a89cf6b19a2d16f58838a3a8b07
S.V.Tharunn <tharunn030103@gmail.com>
2025-07-08 01:36:59 +0530

    solutions updated with explanation

--- 2149.cpp
+++ 2149.cpp
@@ -1,6 +1,11 @@
+// brief explanation.
+//     the idea is to initialize a static vector. have 2 pointers which shows positive and negative values in the array index, which is incremented by +2 (for alternative numbers.) everytime the number is added, the pointer moves 2 steps ahead, filling the static array/vector.
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
+// this function prints any array given.
 void print(vector<int> v)
 {
     for (int i : v)
@@ -10,29 +15,29 @@ void print(vector<int> v)
     cout << endl;
 }
 
+
 int main()
 {
+    // sample array for the question.
     vector<int> nums = {28,-41,22,-8,-37,46,35,-9,18,-6,19,-26,-37,-10,-9,15,14,31};
 
-    int pos = 0; int neg = 1;
+    // answer logic starts here.
+    int pos = 0; int neg = 1;                    // two pointers
+    vector<int> ans(nums.size(), 0);             // resulting vector
 
-    vector<int> ans(nums.size(), 0);
-
-    for (int i = 0 ; i < nums.size(); i++)
+    for (int i = 0 ; i < nums.size(); i++)       // for loop through the given vector
     {
         if (nums[i] > 0)
         {
             ans[pos] = nums[i];
-            pos += 2;
+            pos += 2;                            // incrementing the pointer by 2
         }
         else
         {
             ans[neg] = nums[i];
-            neg += 2;
+            neg += 2;                            // incrementing the pointer by 2
         }
     }
-    print(ans);
-
-    
+    print(ans);    
     return 0;
 }
